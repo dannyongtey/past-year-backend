39cbb84710fbd477725566e2279d69da83e6c683
Danny Tey <dannyongtey@gmail.com>
2019-06-30 21:15:49 +0800

    feat: Check for domain in middleware to avoid abuse

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -7,15 +7,18 @@ export default async function (req, res, next) {
     if (req.method !== 'OPTIONS') {
         const authHeader = JSON.parse(req.header('authorization').split('Bearer ')[1])
         const { tokenId } = authHeader
-        console.log(tokenId)
         try {
             const ticket = await client.verifyIdToken({
                 idToken: tokenId,
                 audience: GOOGLE_CLIENT_ID,
             });
             const payload = ticket.getPayload();
-            console.log(payload)
-            next()
+            if (payload['hd'] !== 'student.mmu.edu.my'){
+                res.status(401).json({error: 'Unauthorized. Please use MMU student email to login.'})
+                return
+            } else {
+                next()
+            }
         } catch {
             res.status(401).json({error: 'Unauthorized. Please ensure you are logged in using official MMU Account.'})
             return
