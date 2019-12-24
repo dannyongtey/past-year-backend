6a7d4a983fcecfa563862d8ac4ac85cf932ba6c4
Danny Tey <dannyongtey@gmail.com>
2019-12-24 21:13:34 +0800

    Add ram limit

--- src/jobs/index.js
+++ src/jobs/index.js
@@ -6,7 +6,7 @@ import schedule from 'node-schedule'
 
 const filePath = 'meta.json'
 const papersPath = process.env.storage_path || '/tmp'
-schedule.scheduleJob('00 * * * *', function () {
+schedule.scheduleJob('01 * * * *', function () {
     scrapeAllInformation()
 });
 
